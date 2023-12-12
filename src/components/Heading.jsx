07de72620ecd09f98c6b8936af8884a2650d0b64
jamshed <jamshed2019@namal.edu.pk>
2023-12-12 15:22:39 +0500

    react task

---
+++ src/components/Heading.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Heading = () => {
+  return (
+    <>
+    <h1>My Favorite Personality</h1>
+    </>
+  )
+}
+export default Heading;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <>
+    <img className='picture' src="https://images.pexels.com/photos/18272969/pexels-photo-18272969/free-photo-of-lake-and-snow-on-rocks-behind.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt=""  /></>
+  )
+}
+export default Nav;
